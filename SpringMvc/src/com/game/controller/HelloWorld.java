2362a8e50cd0e7f868d7abfa6193073be0443ba9
Guojiuhe <35914193+guojiuhe@users.noreply.github.com>
2018-03-21 13:35:21 +0800

    commit code

---
+++ SpringMvc/src/com/game/controller/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.game.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+	
+    @RequestMapping("/helloworld")  // 此处控制浏览器里访问路径 具体为：/SpringMvc/helloworld
+    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+        //输出字符串
+        response.getWriter().append("hello world");
+
+    }
+}
